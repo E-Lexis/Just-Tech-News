289d527adf58783bc9dd2773e6dabe183c0b34f9
Elexis Blash <elexisblash@gmail.com>
2022-04-27 23:53:28 -0400

    Post model and api routes completed

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User {
+    foreignKey: 'user_id'
+});
 
 
 
@@ -6,8 +15,4 @@ const User = require('./User');
 
 
 
-
-
-
-
-module.exports = { User };
+module.exports = { User, Post };
